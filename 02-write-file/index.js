b5e28f52c12c279f320be4f0d3daee0d8cfa4824
Philipp Khromov <filooloog@mail.ru>
2022-05-21 20:07:23 +0300

    feat: done 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout, stdin } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+
+
+const sayBye = () => stdout.write('See you!\n');
+
+process.on('SIGINT', () => {
+  sayBye();
+  process.exit();
+});
+
+stdout.write('Hello! Print some text below:\n');
+
+stdin.on('data', data => {
+  if (data.toString() === 'exit\r\n' || data.toString() === 'exit\n') {
+    sayBye();
+    process.exit();
+  }
+  writeStream.write(data, err => {
+    if (err) throw err;
+  });
+});
