accf427212c379ca95a971b98046d208a03722e3
Philipp Khromov <filooloog@mail.ru>
2022-05-22 16:31:27 +0300

    feat: done 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 
+const dirCopyPath = path.join(__dirname, 'files');
+const dirNewPath = path.join(__dirname, 'files-copy');
+
+const copyDir = async (copyDirPath, newDirPath) => {
+  fs.mkdir(newDirPath, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  // manage new directory
+  const dirNewExistingEntries = await fs.promises.readdir(newDirPath);
+  if (dirNewExistingEntries) {
+    for await (let entry of dirNewExistingEntries) {
+      fs.unlink(path.join(newDirPath, entry), (err) => {
+        if (err) throw err;
+      });
+    }
+  }
+  
+  // copy entries of copied directory
+  const dirCopyEntries = await fs.promises.readdir(copyDirPath);
+  for await (let entry of dirCopyEntries) {
+    fs.copyFile(path.join(copyDirPath, entry), path.join(newDirPath, entry), (err) => {
+      if (err) throw err;
+    });
+  }
+};
+
+copyDir(dirCopyPath, dirNewPath);
