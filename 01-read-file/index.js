d26e7209c88cb123d38f56550f1f8e22002d73c0
Philipp Khromov <filooloog@mail.ru>
2022-05-23 09:54:15 +0300

    fix: rework 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,15 +2,16 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const filePath = path.join('01-read-file', 'text.txt');
-const fileStream = fs.createReadStream(filePath);
+const fileStream = fs.createReadStream(filePath, 'utf-8');
 
 try {
   fileStream.on('error', () => {
     throw new Error('File could not be read');
   });
-  fileStream.on('data', data => {
-    console.log(data);
-  });
 } catch (err) {
   console.error(err);
 }
+
+fileStream.on('data', data => {
+  console.log(data);
+});
