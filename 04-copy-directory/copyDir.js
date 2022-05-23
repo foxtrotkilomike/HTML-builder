6c11a3099dc4e080c4940c7b83205e435d802186
Philipp Khromov <filooloog@mail.ru>
2022-05-23 10:06:38 +0300

    refactor: catch errors 04-copy-directory

--- 04-copy-directory/copyDir.js
+++ 04-copy-directory/copyDir.js
@@ -2,26 +2,52 @@ const fs = require('fs');
 const path = require('path');
 
 const copyDir = async (copyDirPath, newDirPath) => {
-  fs.mkdir(newDirPath, { recursive: true }, (err) => {
-    if (err) throw err;
-  });
+  try {
+    await fs.promises.mkdir(newDirPath, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+  } catch (err) {
+    console.error('Error making dorectory to copy');
+    console.error(err);
+  }
 
   // manage new directory
-  const dirNewExistingEntries = await fs.promises.readdir(newDirPath);
+  const dirNewExistingEntries = await fs.promises.readdir(newDirPath, {withFileTypes: true});
   if (dirNewExistingEntries) {
     for await (let entry of dirNewExistingEntries) {
-      fs.unlink(path.join(newDirPath, entry), (err) => {
-        if (err) throw err;
-      });
+      if(!entry.isDirectory()) {
+        try {
+          fs.unlink(path.join(newDirPath, entry.name), (err) => {
+            if (err) throw err;
+          });
+        } catch (err) {
+          console.error('Error deleting existing files in new directory');
+          console.error(err);
+        }
+      }
     }
   }
   
   // copy entries of copied directory
-  const dirCopyEntries = await fs.promises.readdir(copyDirPath);
+  const dirCopyEntries = await fs.promises.readdir(copyDirPath, {withFileTypes: true});
   for await (let entry of dirCopyEntries) {
-    fs.copyFile(path.join(copyDirPath, entry), path.join(newDirPath, entry), (err) => {
-      if (err) throw err;
-    });
+    if (entry.isDirectory()) {
+      copyDir(
+        path.join(copyDirPath, entry.name), 
+        path.join(newDirPath, entry.name));
+    } else {
+      try {
+        fs.copyFile(
+          path.join(copyDirPath, entry.name), 
+          path.join(newDirPath, entry.name), 
+          (err) => {
+            if (err) throw err;
+          });
+      } catch (err) {
+        console.error('Error copying files to new directory');
+        console.error(err);
+      }
+    }
   }
 };
 
